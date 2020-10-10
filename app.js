9a126ad12bee5d416eb1bd061fc1ecd1f26f6254
Aditya Malik <aditya007@outlook.com>
2020-10-10 10:00:58 +0530

    procfile added for heroku

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ app.post("/filter",function(req,res){
     })
 });
 
-app.listen("5000",function(){
+app.listen(process.env.PORT || "5000",function(){
     console.log("Server is up and running !");
 });
